bfc79ab5cdc6d2d1a3eb9d214af529796db322f9
magicmoo <2321507398@qq.com>
2021-09-30 22:33:48 +0800

    commit

---
+++ #745div2/C.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define CAS int CASE;  \
+            cin>>CASE; \
+            while(CASE--)
+#define YON if(flag) cout<<"YES\n"; \
+            else cout<<"NO\n";
+#define Rep(i,x,y) for(int i=(x);i<=(y);i++)
+#define rep(i,x,y) for(int i=(x);i<(y);i++)
+#define Dep(i,x,y) for(int i=(x);i>=(y);i--)
+#define dep(i,x,y) for(int i=(x);i>(y);i--)
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+typedef long long ll;
+const double PI = acos(-1.0);
+const int INF = 0x3f3f3f3f;
+const int maxn = 500;
+char a[maxn][maxn];
+int pre[maxn][maxn];
+int main(){
+    IOS;
+    int n,m;
+    CAS {
+        cin>>n>>m;
+        Rep(i,1,n){
+            Rep(j,1,m){
+                cin>>a[i][j];
+            }
+        }
+        
+    }
+    return 0;
+}
---
+++ ROUND95/D.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define CAS int CASE;  \
+            cin>>CASE; \
+            while(CASE--)
+#define YON if(flag) cout<<"YES\n"; \
+            else cout<<"NO\n";
+#define Rep(i,x,y) for(int i=(x);i<=(y);i++)
+#define rep(i,x,y) for(int i=(x);i<(y);i++)
+#define Dep(i,x,y) for(int i=(x);i>=(y);i--)
+#define dep(i,x,y) for(int i=(x);i>(y);i--)
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+typedef long long ll;
+const double PI = acos(-1.0);
+const int INF = 0x3f3f3f3f;
+priority_queue<int> q;
+map<int,int> mp;
+set<int> s;
+const int maxn = 1e5+5;
+int a[maxn];
+int main(){
+    IOS;
+    int n,Q;
+    cin>>n>>Q;
+    Rep(i,1,n) cin>>a[i];
+    sort(a+1,a+1+n);
+    Rep(i,1,n) s.insert(a[i]);
+    Rep(i,2,n){
+        q.push(a[i]-a[i-1]);
+    }
+    int cnt = *(--s.end())-*(s.begin());
+    cout<<cnt-q.top()<<"\n";
+    while(Q--){
+        int opr,x;
+        cin>>opr>>x;
+        if(opr==0){
+            auto index = s.lower_bound(x);
+            if(index != s.begin()){
+                mp[(*(index)-*(--index))]++;
+                index++; 
+            }
+            index++;
+            if(index != s.end()) mp[(*index)-*(--index)]++;
+            index = s.erase(index);
+            if(index != s.end() && index != s.begin()) q.push((*index)-*(--index));
+            while(mp[q.top()]){
+                mp[q.top()]--;
+                q.pop();
+            }
+            int cnt = *(--s.end())-*(s.begin());
+            cout<<cnt-q.top()<<"\n";
+        }else{
+            auto index = s.lower_bound(x);
+            if(index!=s.end() && index!=s.begin()) mp[(*(index)-*(--index))]++;
+            s.insert(x);
+            index = s.lower_bound(x);
+            if(index != s.begin()){
+                q.push((*index)-*(--index));
+                index++;
+            }
+            index++;
+            if(index != s.end()) q.push((*index)-*(--index));
+            int cnt = *(--s.end())-*(s.begin());
+            cout<<cnt-q.top()<<"\n";
+        }
+    }
+    return 0;
+}

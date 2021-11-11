bc1f8d2e2b3c2dbc4c75c5bc14b7568efa85e61a
magicmoo <2321507398@qq.com>
2021-11-11 11:49:06 +0800

    commit

--- #626div2/D.cpp
+++ #626div2/D.cpp
@@ -14,9 +14,26 @@ using namespace std;
 typedef long long ll;
 const double PI = acos(-1.0);
 const int INF = 0x3f3f3f3f;
-
+const int maxn = 4e5+5;
+int a[maxn],cnt[32],cnt2[32];
 int main(){
     IOS;
-
+    int n;
+    cin>>n;
+    Rep(i,1,n) cin>>a[i];
+    Rep(i,1,n){
+        rep(j,0,30){
+            cnt[j] += (a[i]>>j)&1;
+        }
+    }
+    Rep(i,1,n){
+        rep(j,0,30){
+            if((a[i]>>j)&1){
+                
+            }
+        }
+    }
+    int ans = 0,now = 1,last=0;
+    cout<<ans<<"\n";
     return 0;
 }
